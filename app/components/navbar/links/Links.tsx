c554a919a7436603fa228c3ee1e7c3a27f8c83cf
ANINDITAPRIYADARSHINI <anindita.priyadarshini8@gmail.com>
2025-01-09 17:57:42 +0530

    Added media queries.

--- app/components/navbar/links/Links.tsx
+++ app/components/navbar/links/Links.tsx
@@ -1,5 +1,10 @@
+"use client"
+
+import React from "react";
 import styles from "./links.module.css";
 import NavLink from "./navLink/NavLink";
+import Link from "next/link";
+import { link } from "fs/promises";
 
 export default function Links() {
 
@@ -25,26 +30,41 @@ export default function Links() {
     },
   ]
 
+  const [open, setOpen] = React.useState(false)
+
   const session = true;
   const isAdmin = true;
 
   return (
-    <div className={styles.links}>
-      {links.map((link => (
-        <NavLink item={link} key={link.title} />
-      )))}
-      
-      {session ? (
-        <>
-          {isAdmin && 
-            <NavLink item={{title: "Admin", path: "/admin"}} />
-          }
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link => (
+          <NavLink item={link} key={link.title} />
+        )))}
+        
+        {session ? (
+          <>
+            {isAdmin && 
+              <NavLink item={{title: "Admin", path: "/admin"}} />
+            }
 
-          <button className={styles.logout}>Logout</button>
-        </>
-      ) : (
-        <NavLink item={{title: "Login", path: "/login"}} />
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{title: "Login", path: "/login"}} />
+        )}
+      </div>
+
+      <button className = {styles.menuButton} onClick={() => setOpen((prev) => !prev)}> Menu </button>
+      {
+        open && (
+          <div className={styles.mobileLinks}>
+            {links.map((link) => (
+              <NavLink item={link} key={link.title} />
+            ))}
+        </div>
       )}
     </div>
+    
   );
 }
