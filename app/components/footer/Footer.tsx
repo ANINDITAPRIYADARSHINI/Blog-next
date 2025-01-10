2ab6de00a9aee4cf72b04c3462bca939b3ce0487
ANINDITAPRIYADARSHINI <anindita.priyadarshini8@gmail.com>
2025-01-10 23:05:49 +0530

    Added footer layout with its media query.

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,7 +1,10 @@
+import styles from "./footer.module.css";
+
 export default function Footer() {
     return (
-      <div>
-        <h1>Footer</h1>
+      <div className={styles.container}>
+        <div className={styles.logo}>Anindita</div>
+        <div className={styles.text}>Creative thoughts agency © All rights reserved.</div>
       </div>
     );
   }
